92c1a2ef3c0552c3be66e579a8ee7595789fba21
Akeloma <150233471+Akeloma@users.noreply.github.com>
2025-07-17 16:32:38 +0800

    changing header location

--- flt_detailed.py
+++ flt_detailed.py
@@ -129,7 +129,7 @@ def main():
 
     # Title rows
     ws["A64"] = "Group FLT Details"
-    ws["M64"] = "Regional/Local FLT Details"
+    ws["O64"] = "Regional/Local FLT Details"
     for cell in [ws["A64"], ws["M64"]]:
         cell.fill = PatternFill("solid", fgColor="00B0F0")
         cell.font = Font(bold=True, color="FFFFFF")
